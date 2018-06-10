7eab17775939f1a443eb9170def4e06e1bee2fd1
Sarah <sarahwbcodes@gmail.com>
2018-06-10 10:56:47 +0300

    added shelf compnents in render()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import Shelf from './src/Shelf'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    query:'',
+    books: [],
   }
+
+  /*componentDidMount(){
+    (api)=> api.getAll().then(books)=>{
+    this.setState({books})
+  }};*/
+
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+      <div>
+      <Shelf title={"Fantasy"}/>
+      <Shelf title={"Fiction"}/>
+      <Shelf title={"Management"}/>
+      </div>
+        {this.state.showSearchPage ?(
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
