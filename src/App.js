70d6e33754b277c20c1c15ee76ee824772bcab28
Sarah <sarahwbcodes@gmail.com>
2018-06-13 11:01:14 +0300

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import * as BookData from './BooksAPI';
 import Shelf from './components/Shelf';
-import  {Route} from 'react-router-dom';
+import  {Route,Link} from 'react-router-dom';
 import './App.css';
+import PropTypes from 'prop-types';
+import Search from './components/Search';
 
 export default class BooksApp extends React.Component {
 constructor(){
   super();
 }
+//to ensure everything's in their right place. books, as seen in state, are required to be arrays in order to work.
+// and for the shelf change/switch to occur, then shelfswitch should work as a function. 
+static propTypes ={
+  books: PropTypes.array.isRequired,
+  shelfSwitch: PropTypes.func.isRequired
+}
+//states are left blank, data is added later through the component did mount and update
 state = {
   books: []
 }
+//to get data from the API and enable its manipulation as it is not static HTML but fetched from
+//the books JSON. Here the books fetched are set as the state of the component to ensure them showing up on the UI.
   componentDidMount(){
     BookData.getAll()
     .then((books) => {
       this.setState({books});
       console.log(books);
     });
+    //console logs are also another precaution to check if everything's working smoothly
     console.log(this.state.books);
 }
-
-
-/*componentWillUpdate(){
-  BooksAPI.getAll()
+//this ensures the UI is up to date with any changes that may occur on props or state
+componentWillUpdate(){
+  BookData.getAll()
   .then((books) => {
     this.setState({books});
     console.log(books);
   });
-}*/
-
-shelfSwitch = (book, shelf)=>{
-  BookData.update(book, shelf)
-  .then(book.shelf = shelf)
 }
 
-booksByShelf = (shelf) => this.state.books.filter((book)=> book.shelf === shelf)
+// We take two parameters, book we want to update, & the new shelf
+shelfSwitch = (book,shelf) => {
+  // You can view the implmentation in BookAPI.js
+  BookData.update(book,shelf)
+}
+
+//a function that is an extension of the search component, if it even works rn. 
+/*lookUpBook= () =>{
+  BookData.search(query).then
+}*/
+
+// a function to filter out the book arrays so each will reside in their respective shelves
+booksByShelf = (shelf) => this.state.books.filter((book) => book.shelf === shelf)
 
   render() {
     return (
@@ -43,23 +61,27 @@ booksByShelf = (shelf) => this.state.books.filter((book)=> book.shelf === shelf)
     <h1>MyReads</h1>
   </div>
   <div className="list-books-content">
-    <div>
+    
     <Route exact path="/" render={()=>(
-  <Shelf title={"Currently Reading"} books={booksByShelf('currentlyReading')}/>
-  <Shelf title={"Want to Read"} books={booksByShelf('wantToRead')}/>
-  <Shelf title={"Read"} books={booksByShelf('read')}/>
-)} />
-        </div>
+      <div>
+  <Shelf title={"Currently Reading"} books={this.booksByShelf('currentlyReading')} shelfswitch={this.shelfSwitch}/>
+  <Shelf title={"Want to Read"} books={this.booksByShelf('wantToRead')} shelfswitch={this.shelfSwitch}/>
+  <Shelf title={"Read"} books={this.booksByShelf('read')} shelfswitch={this.shelfSwitch}/>
+  </div>
+)}/>
+        <div className='open-search'>
+        <Link to={`/search`}> Search for a book </Link>
+      </div>
       </div>
         <Route exact path="/search" render={()=>(
           <div>
           <Search
           query={this.state.query}
-          results={this.updateQuery}
+          bookResults={this.searchUpdate}
           shelfSwitch={this.shelfSwitch}
           />
           </div>
-        )}
+        )}/>
         </div>
     )}
 }
