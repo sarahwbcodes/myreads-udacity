333ad2621a3a76ed8043fa2bcb701c08f02eb319
Sarah <sarahwbcodes@gmail.com>
2018-06-13 11:02:16 +0300

    added comments

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,10 @@
 import React from 'react'
 export default class Book extends React.Component{
 
+  // Here, we want the select option changed, we get the value of it and pass it to the parent
+handleChange = (event) => {
+  this.props.shelfswitch(this.props.content, event.target.value)
+}
   render(){
     //const bookOrder=this.props.state.book.sort(sortBy('title'))
     return(
@@ -8,7 +12,7 @@ export default class Book extends React.Component{
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+`${this.props.content.imageLinks.smallThumbnail}`+')'}}></div>
                       <div className="book-shelf-changer">
-                        <select book={this.props.book} value={this.state.shelf} shelfSwitch={this.shelfSwitch}>
+                        <select book={this.props.content} onChange={this.handleChange} >
                           <option value="move" disabled>Move to...</option>
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
