b0ad111dedf977f187189af115dab92920a18c82
Sarah <sarahwbcodes@gmail.com>
2018-06-14 12:38:06 +0300

    switched search functionality
    commented it out and went back to the old one

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,60 +1,96 @@
 import React from 'react';
-import  {Link} from 'react-router-dom';
+import {
+  Link
+} from 'react-router-dom';
 import sortBy from 'sort-by'
-import escapeRegExp from 'escape-string-regexp'
+//import escapeRegExp from 'escape-string-regexp'
 import Book from './Book'
-import * as BookData from '../BooksAPI'
+//import * as BookData from '../BooksAPI'
 
 
-export default class Search extends React.Component{
-    state={
-        query:''
+export default class Search extends React.Component {
+    state = {
+      query: '',
+      books: []
     }
-    
+
     searchUpdate = (query) => {
-        this.setState({query: query.trim()})
+      this.setState({
+        query: query.trim()
+      })
     }
-    
+
     searchClear = () => {
-        this.setState({query:''})
+      this.setState({
+        query: ''
+      })
     }
-    render(){
-        //search and result functionality courtesy of Udacity's React lessons
-        const {query} = this.state
-        const {books} = this.props
-        let bookResults
-        if (query){
-            //so we can ignore any special characters and pass them as object literals
+    render() {
+      //search and result functionality courtesy of Udacity's React lessons (edited since then)
+      const {
+        query
+      } = this.state
+      const {
+        books
+      } = this.props
+      //lookUpBook = (query) => BookData.search()
+      let bookResults
+      if (query) {
+        //so we can ignore any special characters and pass them as object literals
         const result = new RegExp(escapeRegExp(query), 'i')
-        bookResults = books.filter((book)=> result.test(book.title))
-        }else{
-            bookResults = books
-        }
-        //bookResults.sort(sortBy('title'))
-        
-        return(
-      <div className="search-books">
-      <div className="search-books-bar">
-      <Link to={`/`} className="close-search">Close</Link>
-      <div className="search-books-input-wrapper">
-      <input type='text'
-      value={query}
-      onChange={(event) => this.searchUpdate(event.target.value)}
-      placeholder="Search by a book title or author name"
-      />
-      <button onClick={this.searchClear}>Show all books</button>
-      </div>
-      </div>
-      <div className="search-book-results">
-      <ol className="books-grid">
-      {bookResults.map((book)=>(
-        <li key={book.id}>
-        <Book content={book} shelfSwitch={this.shelfSwitch}/>
-        </li>
-      ))
+        bookResults = books.filter((book) => result.test(book.title))
+      } else {
+        bookResults = books
+        /*return <div >
+        < span > "No books found" < /span>
+        <span onClick={this.searchClear}>"try again"</span >
+        < span > "or" < /span>
+        <Link to={`\`}><span>"go back"</span>< /Link>
+        </div>*/
       }
-      </ol>
-      </div>
-      </div>
-        )}
+      bookResults.sort(sortBy('title')
+      return ( <
+          div className = "search-books" >
+          <
+          div className = "search-books-bar" >
+          <
+          Link to = {
+            `/`
+          }
+          className = "close-search" > Close < /Link> <
+          div className = "search-books-input-wrapper" >
+          <
+          input type = 'text'
+          value = {
+            query
+          }
+          onChange = {
+            (event) => this.searchUpdate(event.target.value)
+          }
+          placeholder = "Look up a book through its title or its author" /
+          >
+          <
+          /div> <
+          div className = "search-book-results" >
+          <
+          ol className = "books-grid" > {
+            bookResults.map((book) => ( <
+              li key = {
+                book.id
+              } >
+              <
+              Book content = {
+                book
+              }
+              shelfSwitch = {
+                this.shelfSwitch
+              }
+              /> <
+              /li>
+            ))
+          } <
+          /ol> <
+          /div>
+        )
       }
+    }
