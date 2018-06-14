d2741d61c2a9a8d7f5bf22febae4171bfd63ff07
Sarah <sarahwbcodes@gmail.com>
2018-06-14 12:38:29 +0300

    code indentation
    Atom Beautify

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import Book from './Book'
 
-export default class Shelf extends React.Component{
+export default class Shelf extends React.Component {
+  render() {
+    const {
+      books
+    } = this.props
+    return ( <
+      div className = "bookshelf" >
+      <
+      h2 className = "bookshelf-title" > {
+        this.props.title
+      } < /h2> <
+      div className = "bookshelf-books" >
+      <
+      ol className = "books-grid" > {
+        books.map((book) => ( <
+          li key = {
+            book.id
+          } >
+          <
+          Book content = {
+            book
+          }
+          shelfswitch = {
+            this.props.shelfswitch
+          }
+          /> <
+          /li>
+        ))
+      }
 
-  render(){
-    return(
-    <div className="bookshelf">
-    <h2 className="bookshelf-title">{this.props.title}</h2>
-    <div className="bookshelf-books">
-    <ol className="books-grid">
-{this.props.books.map((book)=>(
-  <li key={book.id}>
-  <Book content={book} shelfswitch={this.props.shelfswitch}/>
-  </li>
-))}
-
-     </ol>
-  </div>
-   </div>
+      <
+      /ol> <
+      /div> <
+      /div>
 
 
     )
