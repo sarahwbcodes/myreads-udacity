6635edf04f1862855b07a3de754cf36f27f52fd2
Sarah <sarahwbcodes@gmail.com>
2018-06-14 13:13:24 +0300

    removed the additional books array & api search arrow function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,96 +1,78 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import  {Link} from 'react-router-dom';
 import sortBy from 'sort-by'
-//import escapeRegExp from 'escape-string-regexp'
+import escapeRegExp from 'escape-string-regexp'
 import Book from './Book'
-//import * as BookData from '../BooksAPI'
+import * as BookData from '../BooksAPI'
 
 
-export default class Search extends React.Component {
-    state = {
-      query: '',
-      books: []
+export default class Search extends React.Component{
+    state={
+        query:''
     }
 
     searchUpdate = (query) => {
-      this.setState({
-        query: query.trim()
-      })
+        this.setState({query: query.trim()})
     }
 
     searchClear = () => {
-      this.setState({
-        query: ''
-      })
+        this.setState({query:''})
     }
-    render() {
-      //search and result functionality courtesy of Udacity's React lessons (edited since then)
-      const {
-        query
-      } = this.state
-      const {
+    //search and result functionality courtesy of Udacity's React lessons
+    //search through book data
+    
+    const {query}= this.state
+    const {books} = this.props
+
+    BookData.search(query).then(books => books ? this.setState({
         books
-      } = this.props
-      //lookUpBook = (query) => BookData.search()
-      let bookResults
-      if (query) {
+      }) : []);
+      this.setState({
+       query
+      });
+    
+//or
+    /*BookData.search(query).then((books)=>
+    if (query===books){
+    this.setState({results:books})
+  )}
+  else {
+    this.setState({results:[]})
+  }*/
+    //display the books
+    let bookResults
+    if (query){
         //so we can ignore any special characters and pass them as object literals
-        const result = new RegExp(escapeRegExp(query), 'i')
-        bookResults = books.filter((book) => result.test(book.title))
-      } else {
+    const result = new RegExp(escapeRegExp(query), 'i')
+    bookResults = books.filter((book)=> result.test(book.title))
+    }else{
         bookResults = books
-        /*return <div >
-        < span > "No books found" < /span>
-        <span onClick={this.searchClear}>"try again"</span >
-        < span > "or" < /span>
-        <Link to={`\`}><span>"go back"</span>< /Link>
-        </div>*/
-      }
-      bookResults.sort(sortBy('title')
-      return ( <
-          div className = "search-books" >
-          <
-          div className = "search-books-bar" >
-          <
-          Link to = {
-            `/`
-          }
-          className = "close-search" > Close < /Link> <
-          div className = "search-books-input-wrapper" >
-          <
-          input type = 'text'
-          value = {
-            query
-          }
-          onChange = {
-            (event) => this.searchUpdate(event.target.value)
-          }
-          placeholder = "Look up a book through its title or its author" /
-          >
-          <
-          /div> <
-          div className = "search-book-results" >
-          <
-          ol className = "books-grid" > {
-            bookResults.map((book) => ( <
-              li key = {
-                book.id
-              } >
-              <
-              Book content = {
-                book
-              }
-              shelfSwitch = {
-                this.shelfSwitch
-              }
-              /> <
-              /li>
-            ))
-          } <
-          /ol> <
-          /div>
-        )
-      }
     }
+    render(){
+        bookResults.sort(sortBy('title'))
+    return(
+      <div className="search-books">
+      <div className="search-books-bar">
+      <Link to={`/`} className="close-search">Close</Link>
+      <div className="search-books-input-wrapper">
+      <input type='text'
+      value={query}
+      onChange={(event) => this.searchUpdate(event.target.value)}
+      placeholder="Search by a book title or author name"
+      />
+      <button onClick={this.searchClear}>Show all books</button>
+      </div>
+      </div>
+      <div className="search-book-results">
+      <ol className="books-grid">
+      {bookResults.map((book)=>(
+        <li key={book.id}>
+        <Book content={book} shelfSwitch={this.shelfSwitch}/>
+        </li>
+      ))
+      }
+      </ol>
+      </div>
+      </div>
+        )}
+      
